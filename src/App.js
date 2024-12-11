2a8d856885ee8b99c658ff57b1f46674f230045d
ismael840 <ismaelronaldoflores840@gmail.com>
2024-12-11 02:00:31 -0300

    cambio de logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import ruleta from "./ruleta.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          Timba
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </h2>
+        <img
+          src={ruleta}
+          className="App-logo"
+          alt="logo"
+          // style={{ paddingBottom: "20px" }}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ paddingTop: "20px" }}
         >
-          Learn React
+          Intermedio
         </a>
       </header>
     </div>
